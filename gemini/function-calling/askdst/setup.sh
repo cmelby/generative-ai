c53720ecf05de420615c3179cda8622ccba3781d
Chris Melby <56492120+cmelby@users.noreply.github.com>
2024-05-17 11:12:57 -0700

    Update setup.sh

--- gemini/function-calling/askdst/setup.sh
+++ gemini/function-calling/askdst/setup.sh
@@ -5,16 +5,16 @@ gcloud services enable aiplatform.googleapis.com
 gcloud services enable bigquery.googleapis.com
 
 # Copy public dataset
-bq mk --force=true --dataset thelook_ecommerce
+bq mk --force=true --dataset performanceOverview
 bq mk \
   --transfer_config \
   --data_source=cross_region_copy \
-  --target_dataset=thelook_ecommerce \
-  --display_name='askDST' \
+  --target_dataset=performanceOverview \
+  --display_name='AskDST' \
   --schedule_end_time="$(date -u -d '5 mins' +%Y-%m-%dT%H:%M:%SZ)" \
   --params='{
-      "source_project_id":"dst-master",
-      "source_dataset_id":"dst_gpsell"
+      "source_project_id":"google.com:lps-global-sales-engine-243309",
+      "source_dataset_id":"google.com:lps-global-sales-engine-243309.performanceOverview"
       }'
 
 # Install Python
@@ -28,3 +28,4 @@ ${PYTHON_PREFIX}/bin/pip install -r requirements.txt
 
 # Run app
 ${PYTHON_PREFIX}/bin/streamlit run app.py --server.enableCORS=false --server.enableXsrfProtection=false --server.port 8080
+
